6b636f3e8d92fb4e74d50f488164662550f0877e
Neha <soni.neha2191@gmail.com>
2018-07-13 02:19:32 -0700

    Delete Dictionaries.py

--- Dictionaries.py
+++
@@ -1,3 +0,0 @@
-# Dictionaries
-# Unlike Sets and lists, dictionaries store pairs of elements: keys and values.
-# E.g. Consider a dictionary with keys are element names and the values are their corresponding atomic numbers.
